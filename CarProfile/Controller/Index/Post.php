3adb71b683fb2e3b37b0647dd31ab8a72addc300
UpSage <aton.christopher05@gmail.com>
2024-10-06 20:06:25 +0000

    Removed Logger

--- CarProfile/Controller/Index/Post.php
+++ CarProfile/Controller/Index/Post.php
@@ -8,26 +8,22 @@ use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Session\SessionManagerInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 use Razoyo\CarProfile\Model\CarProfileFactory;
-use Psr\Log\LoggerInterface;
 
 class Post extends Action
 {
     protected $coreSession;
     protected $customerSession;
     protected $carProfileFactory;
-    protected $logger;
 
     public function __construct(
         Context $context,
         SessionManagerInterface $coreSession,
         CustomerSession $customerSession,
-        CarProfileFactory $carProfileFactory,
-        LoggerInterface $logger
+        CarProfileFactory $carProfileFactory
     ) {
         $this->coreSession = $coreSession;
         $this->customerSession = $customerSession;
         $this->carProfileFactory = $carProfileFactory;
-        $this->logger = $logger;
         parent::__construct($context);
     }
 
@@ -36,48 +32,39 @@ class Post extends Action
         $postData = $this->getRequest()->getPostValue();
 
         if ($postData) {
-            // Validation: Check if car_make and car_model are empty
             if (empty($postData['car_make']) || empty($postData['car_model'])) {
                 $this->messageManager->addErrorMessage(__('Car make and Car model are required fields.'));
-                return $this->_redirect('*/*/'); // Redirect back to the form
+                return $this->_redirect('*/*/');
             }
 
             try {
                 $customerId = $this->customerSession->getCustomerId();
-                $this->logger->debug('Customer ID: ' . $customerId);
 
                 if (!$customerId) {
                     $this->messageManager->addErrorMessage(__('Unable to find customer.'));
                     return $this->_redirect('*/*/');
                 }
 
-                // Load existing profile by customer ID
                 $carProfile = $this->carProfileFactory->create();
                 $carProfile->load($customerId, 'customer_id');
 
                 if (!$carProfile->getId()) {
-                    // If no profile exists, create a new one
                     $carProfile->setData([
                         'customer_id' => $customerId,
                         'car_make' => $postData['car_make'],
                         'car_model' => $postData['car_model'],
                         'created_at' => date('Y-m-d H:i:s')
                     ]);
-                    $this->logger->debug('Creating new car profile for customer ID: ' . $customerId);
                 } else {
-                    // Update existing profile
                     $carProfile->setCarMake($postData['car_make']);
                     $carProfile->setCarModel($postData['car_model']);
                     $carProfile->setCreatedAt(date('Y-m-d H:i:s'));
-                    $this->logger->debug('Updating existing car profile for customer ID: ' . $customerId);
                 }
 
-                // Save the car profile
                 $carProfile->save();
                 $this->messageManager->addSuccessMessage(__('Your car profile has been submitted.'));
 
             } catch (\Exception $e) {
-                $this->logger->error('Error saving car profile for customer ID ' . $customerId . ': ' . $e->getMessage());
                 $this->messageManager->addErrorMessage(__('Unable to save your car profile: ' . $e->getMessage()));
             }
 
