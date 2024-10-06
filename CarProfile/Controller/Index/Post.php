d6bc4bb4837ed13e7382af52f75533e01be2eaab
UpSage <aton.christopher05@gmail.com>
2024-10-06 23:24:15 +0000

    Test Ajax header response

--- CarProfile/Controller/Index/Post.php
+++ CarProfile/Controller/Index/Post.php
@@ -4,20 +4,23 @@ namespace Razoyo\CarProfile\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Customer\Model\Session as CustomerSession;
 use Razoyo\CarProfile\Model\CarProfileFactory;
 
 class Post extends Action
 {
+    protected $jsonFactory;
     protected $customerSession;
     protected $carProfileFactory;
 
     public function __construct(
         Context $context,
+        JsonFactory $jsonFactory,
         CustomerSession $customerSession,
         CarProfileFactory $carProfileFactory
     ) {
+        $this->jsonFactory = $jsonFactory;
         $this->customerSession = $customerSession;
         $this->carProfileFactory = $carProfileFactory;
         parent::__construct($context);
@@ -25,20 +28,25 @@ class Post extends Action
 
     public function execute()
     {
+        $resultJson = $this->jsonFactory->create();
         $postData = $this->getRequest()->getPostValue();
 
         if ($postData) {
             if (empty($postData['car_make']) || empty($postData['car_model'])) {
-                $this->messageManager->addErrorMessage(__('Car make and Car model are required fields.'));
-                return $this->_redirect('*/*/');
+                return $resultJson->setData([
+                    'success' => false,
+                    'message' => __('Car make and Car model are required fields.')
+                ]);
             }
 
             try {
                 $customerId = $this->customerSession->getCustomerId();
 
                 if (!$customerId) {
-                    $this->messageManager->addErrorMessage(__('Unable to find customer.'));
-                    return $this->_redirect('*/*/');
+                    return $resultJson->setData([
+                        'success' => false,
+                        'message' => __('Unable to find customer.')
+                    ]);
                 }
 
                 $carProfile = $this->carProfileFactory->create();
@@ -58,16 +66,25 @@ class Post extends Action
                 }
 
                 $carProfile->save();
-                $this->messageManager->addSuccessMessage(__('Your car profile has been submitted.'));
+
+                return $resultJson->setData([
+                    'success' => true,
+                    'message' => __('Your car profile has been submitted.'),
+                    'car_make' => $postData['car_make'],
+                    'car_model' => $postData['car_model']
+                ]);
 
             } catch (\Exception $e) {
-                $this->messageManager->addErrorMessage(__('Unable to save your car profile: ' . $e->getMessage()));
+                return $resultJson->setData([
+                    'success' => false,
+                    'message' => __('Unable to save your car profile: ' . $e->getMessage())
+                ]);
             }
-
-            return $this->_redirect('*/*/');
         }
 
-        $this->messageManager->addErrorMessage(__('No data received.'));
-        return $this->_redirect('*/*/');
+        return $resultJson->setData([
+            'success' => false,
+            'message' => __('No data received.')
+        ]);
     }
 }
